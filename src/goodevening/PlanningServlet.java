2dcbe76c310e373023ae192c8712d1f1c5fa0563
ivanightingale <guancheq@usc.edu>
2018-11-04 18:10:38 -0800

    started eclipse project

---
+++ src/goodevening/PlanningServlet.java
@@ -0,0 +1,26 @@
+package goodevening;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PlanningServlet
+ */
+@WebServlet("/PlanningServlet")
+public class PlanningServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PlanningServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+}
