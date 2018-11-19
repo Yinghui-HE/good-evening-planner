2eb03e5170806cb00a58eebfe2068b7d1e931b33
ivanightingale <guancheq@usc.edu>
2018-11-18 20:02:09 -0800

    Modify category tags, add pictureURL

--- src/goodevening/Servlet.java
+++ src/goodevening/Servlet.java
@@ -220,6 +220,7 @@ public class Servlet extends HttpServlet {
 						if(rs.getInt("timeDependant") == 1) timeDependent = true;
 	    				Event e = new Event(rs.getInt("eventID"),
 											rs.getString("title"),
+											rs.getString("pictureURL"),
 											rs.getInt("startTime"),
 											rs.getInt("endTime"),
 											rs.getInt("duration"),
